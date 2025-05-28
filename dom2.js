0334f6e165b348f18df22cb54c3f64595d5a8e34
Krish Tiwari <krish.tiwari@example.com>
2025-05-28 19:40:43 +0530

    update

--- dom2.js
+++ dom2.js
@@ -3,7 +3,7 @@ let show=()=>{
     let inp1=document.querySelector("#intext1").value  
      let inp2=document.querySelector("#intext2").value  
     let show=document.querySelector("#show")
-
+// adding 2 numbers
     show.innerHTML=Number(inp1)+Number(inp2)
 
 }
